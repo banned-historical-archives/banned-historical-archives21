1a123d718fc6ef227a3af53e11401f6372d9faeb
Your Name <you@example.com>
2024-08-11 12:30:51 +0800

    1

---
+++ cf28d52f-24ed-4a81-911b-d36495068c12.ts
@@ -0,0 +1,43 @@
+export default {
+  "entity": {
+    "id": "cf28d52f-24ed-4a81-911b-d36495068c12",
+    "name": "林副主席的指示——把学习毛主席著作提高到新阶段",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "林彪",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/cf28d52f-24ed-4a81-911b-d36495068c12/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/cf28d52f-24ed-4a81-911b-d36495068c12/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/cf28d52f-24ed-4a81-911b-d36495068c12/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/cf28d52f-24ed-4a81-911b-d36495068c12/4.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "林副主席的指示——把学习毛主席著作提高到新阶段",
+        "authors": [
+          "林彪"
+        ],
+        "page_start": 1,
+        "page_end": 4,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": []
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "cf28d52f-24ed-4a81-911b-d36495068c12",
+  "resource_type": "book",
+  "version": 2
+}
