b4af9b809713f8e9c16eda525247d8c1f7f27582
Your Name <you@example.com>
2024-08-11 11:20:33 +0800

    1

---
+++ fc167846-e1d5-4f6a-8bb0-8af61af6b06b.ts
@@ -0,0 +1,50 @@
+export default {
+  "entity": {
+    "id": "fc167846-e1d5-4f6a-8bb0-8af61af6b06b",
+    "name": "关锋、王力、戚本禹等三同志于10月18日在政协礼堂接见全国各地来京革命师生代表会议上的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "关锋 王力 戚本禹",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/fc167846-e1d5-4f6a-8bb0-8af61af6b06b/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/fc167846-e1d5-4f6a-8bb0-8af61af6b06b/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/fc167846-e1d5-4f6a-8bb0-8af61af6b06b/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "关锋、王力、戚本禹等三同志于10月18日在政协礼堂接见全国各地来京革命师生代表会议上的讲话",
+        "authors": [
+          "关锋",
+          "王力",
+          "戚本禹"
+        ],
+        "page_start": 1,
+        "page_end": 3,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 10,
+            "day": 18
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "fc167846-e1d5-4f6a-8bb0-8af61af6b06b",
+  "resource_type": "book",
+  "version": 2
+}
