70506019c72ba4b22d93aa134ab0a75a0eced028
Your Name <you@example.com>
2024-08-11 20:16:44 +0800

    1

---
+++ 71c093f9-2aa4-4946-a7aa-bfe2075a849c.ts
@@ -0,0 +1,46 @@
+export default {
+  "entity": {
+    "id": "71c093f9-2aa4-4946-a7aa-bfe2075a849c",
+    "name": "上海市革命委员会给上海柴油机厂“联司”革命群众的一封公开信",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "上海市革命委员会",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/71c093f9-2aa4-4946-a7aa-bfe2075a849c/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "上海市革命委员会给上海柴油机厂“联司”革命群众的一封公开信",
+        "authors": [
+          "上海市革命委员会"
+        ],
+        "page_start": 1,
+        "page_end": 1,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1967,
+            "month": 8,
+            "day": 2
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "71c093f9-2aa4-4946-a7aa-bfe2075a849c",
+  "resource_type": "book",
+  "version": 2
+}
