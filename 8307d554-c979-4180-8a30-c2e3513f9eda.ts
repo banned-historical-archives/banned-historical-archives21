54ea2eec1640bfa7460bb4ccdf59222613b2c6e2
Your Name <you@example.com>
2024-08-11 19:40:54 +0800

    1

---
+++ 8307d554-c979-4180-8a30-c2e3513f9eda.ts
@@ -0,0 +1,47 @@
+export default {
+  "entity": {
+    "id": "8307d554-c979-4180-8a30-c2e3513f9eda",
+    "name": "澄清事实",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "张维维 捍卫毛泽东思想工人赤卫队十二分部",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8307d554-c979-4180-8a30-c2e3513f9eda/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "澄清事实",
+        "authors": [
+          "张维维",
+          "捍卫毛泽东思想工人赤卫队十二分部"
+        ],
+        "page_start": 1,
+        "page_end": 1,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 12,
+            "day": 31
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "8307d554-c979-4180-8a30-c2e3513f9eda",
+  "resource_type": "book",
+  "version": 2
+}
