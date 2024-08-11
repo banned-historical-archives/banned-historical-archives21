b926e16c28656ec6a67aa7830a129048a10a1889
Your Name <you@example.com>
2024-08-11 20:10:03 +0800

    1

---
+++ 49c8ec91-7587-45b2-b837-4df9b86e9f28.ts
@@ -0,0 +1,44 @@
+export default {
+  "entity": {
+    "id": "49c8ec91-7587-45b2-b837-4df9b86e9f28",
+    "name": "联合紧急通令",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/49c8ec91-7587-45b2-b837-4df9b86e9f28/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "联合紧急通令",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1967,
+            "month": 1,
+            "day": 28
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "49c8ec91-7587-45b2-b837-4df9b86e9f28",
+  "resource_type": "book",
+  "version": 2
+}
