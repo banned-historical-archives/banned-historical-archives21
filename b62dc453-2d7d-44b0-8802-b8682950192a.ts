61a2a539c48b90073be3f1b50170e6e74c1419d0
Your Name <you@example.com>
2024-08-11 12:50:39 +0800

    1

---
+++ b62dc453-2d7d-44b0-8802-b8682950192a.ts
@@ -0,0 +1,57 @@
+export default {
+  "entity": {
+    "id": "b62dc453-2d7d-44b0-8802-b8682950192a",
+    "name": "一九六六年九月十日在外出串联誓师大会上周恩来总理的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "周恩来",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/b62dc453-2d7d-44b0-8802-b8682950192a/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/b62dc453-2d7d-44b0-8802-b8682950192a/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/b62dc453-2d7d-44b0-8802-b8682950192a/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/b62dc453-2d7d-44b0-8802-b8682950192a/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/b62dc453-2d7d-44b0-8802-b8682950192a/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/b62dc453-2d7d-44b0-8802-b8682950192a/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/b62dc453-2d7d-44b0-8802-b8682950192a/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/b62dc453-2d7d-44b0-8802-b8682950192a/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/b62dc453-2d7d-44b0-8802-b8682950192a/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/b62dc453-2d7d-44b0-8802-b8682950192a/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/b62dc453-2d7d-44b0-8802-b8682950192a/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/b62dc453-2d7d-44b0-8802-b8682950192a/12.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "一九六六年九月十日在外出串联誓师大会上周恩来总理的讲话",
+        "authors": [
+          "周恩来"
+        ],
+        "page_start": 1,
+        "page_end": 12,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 9,
+            "day": 10
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "b62dc453-2d7d-44b0-8802-b8682950192a",
+  "resource_type": "book",
+  "version": 2
+}
