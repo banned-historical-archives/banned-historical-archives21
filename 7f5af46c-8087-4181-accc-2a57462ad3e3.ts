03f3167e7e48eb50ebac2c6a1a935ee1389fc43e
Your Name <you@example.com>
2024-08-11 12:43:03 +0800

    1

---
+++ 7f5af46c-8087-4181-accc-2a57462ad3e3.ts
@@ -0,0 +1,46 @@
+export default {
+  "entity": {
+    "id": "7f5af46c-8087-4181-accc-2a57462ad3e3",
+    "name": "张平化同志八月二十五日讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "张平化",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/7f5af46c-8087-4181-accc-2a57462ad3e3/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "张平化同志八月二十五日讲话",
+        "authors": [
+          "张平化"
+        ],
+        "page_start": 1,
+        "page_end": 1,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 8,
+            "day": 25
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "7f5af46c-8087-4181-accc-2a57462ad3e3",
+  "resource_type": "book",
+  "version": 2
+}
