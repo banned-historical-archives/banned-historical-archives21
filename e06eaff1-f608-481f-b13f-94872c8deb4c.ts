0b21d23a32ec03173f47ab9757300b3cbe18746a
Your Name <you@example.com>
2024-08-12 05:53:58 +0800

    1

---
+++ e06eaff1-f608-481f-b13f-94872c8deb4c.ts
@@ -0,0 +1,18 @@
+export default {
+  "entity": {
+    "id": "e06eaff1-f608-481f-b13f-94872c8deb4c",
+    "name": "中央通知(中央文革小组转发王秀珍关于干部参加劳动的信)",
+    "internal": false,
+    "type": "db",
+    "official": false,
+    "author": "中央文革小组",
+    "files": []
+  },
+  "parser_option": {
+    "articles": []
+  },
+  "parser_id": "result-json",
+  "path": "e06eaff1-f608-481f-b13f-94872c8deb4c.json",
+  "resource_type": "book",
+  "version": 2
+}
