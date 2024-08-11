9895099b9419a2e1b36922445bacfae05649919f
Your Name <you@example.com>
2024-08-11 12:40:32 +0800

    1

---
+++ d106aabf-7562-4574-a8bb-dfabdf5e4a64.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "d106aabf-7562-4574-a8bb-dfabdf5e4a64",
+    "name": "叶剑英同志在接见联络总站工作人员的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "叶剑英",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/d106aabf-7562-4574-a8bb-dfabdf5e4a64/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "叶剑英同志在接见联络总站工作人员的讲话",
+        "authors": [
+          "叶剑英"
+        ],
+        "page_start": 1,
+        "page_end": 1,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": []
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "d106aabf-7562-4574-a8bb-dfabdf5e4a64",
+  "resource_type": "book",
+  "version": 2
+}
