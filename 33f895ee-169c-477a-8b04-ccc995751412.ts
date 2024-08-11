b0e493298523b78379a0e13579e9dd4f29c7f7fe
Your Name <you@example.com>
2024-08-11 13:09:25 +0800

    1

---
+++ 33f895ee-169c-477a-8b04-ccc995751412.ts
@@ -0,0 +1,45 @@
+export default {
+  "entity": {
+    "id": "33f895ee-169c-477a-8b04-ccc995751412",
+    "name": "中央文革接待站访问记——关于目前形势问题",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/33f895ee-169c-477a-8b04-ccc995751412/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/33f895ee-169c-477a-8b04-ccc995751412/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "中央文革接待站访问记——关于目前形势问题",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 2,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 10,
+            "day": 18
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "33f895ee-169c-477a-8b04-ccc995751412",
+  "resource_type": "book",
+  "version": 2
+}
