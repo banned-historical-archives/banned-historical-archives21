187fef4de841549feb6491dd7360257cbd485ced
Your Name <you@example.com>
2024-08-11 13:00:34 +0800

    1

---
+++ 2f4105cb-4afc-4de7-bd1b-ec529c73c852.ts
@@ -0,0 +1,45 @@
+export default {
+  "entity": {
+    "id": "2f4105cb-4afc-4de7-bd1b-ec529c73c852",
+    "name": "周恩来总理在中国科学院辩论大会上的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "周恩来",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/2f4105cb-4afc-4de7-bd1b-ec529c73c852/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/2f4105cb-4afc-4de7-bd1b-ec529c73c852/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/2f4105cb-4afc-4de7-bd1b-ec529c73c852/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/2f4105cb-4afc-4de7-bd1b-ec529c73c852/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/2f4105cb-4afc-4de7-bd1b-ec529c73c852/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/2f4105cb-4afc-4de7-bd1b-ec529c73c852/6.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "周恩来总理在中国科学院辩论大会上的讲话",
+        "authors": [
+          "周恩来"
+        ],
+        "page_start": 1,
+        "page_end": 6,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": []
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "2f4105cb-4afc-4de7-bd1b-ec529c73c852",
+  "resource_type": "book",
+  "version": 2
+}
