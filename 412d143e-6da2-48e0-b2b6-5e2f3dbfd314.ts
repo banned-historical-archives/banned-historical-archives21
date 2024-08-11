9fa049db4b9fb2afa1c027f936b83fbe6ae2f239
Your Name <you@example.com>
2024-08-11 11:24:19 +0800

    1

---
+++ 412d143e-6da2-48e0-b2b6-5e2f3dbfd314.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "412d143e-6da2-48e0-b2b6-5e2f3dbfd314",
+    "name": "解答有关“十六条”中的疑难问题——国务院文革接待站答复",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/412d143e-6da2-48e0-b2b6-5e2f3dbfd314/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/412d143e-6da2-48e0-b2b6-5e2f3dbfd314/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/412d143e-6da2-48e0-b2b6-5e2f3dbfd314/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "解答有关“十六条”中的疑难问题——国务院文革接待站答复",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 3,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": []
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "412d143e-6da2-48e0-b2b6-5e2f3dbfd314",
+  "resource_type": "book",
+  "version": 2
+}
