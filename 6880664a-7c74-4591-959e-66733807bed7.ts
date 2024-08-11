4bcd7002abdd399a5804d8138875da839ed64115
Your Name <you@example.com>
2024-08-11 12:25:42 +0800

    1

---
+++ 6880664a-7c74-4591-959e-66733807bed7.ts
@@ -0,0 +1,49 @@
+export default {
+  "entity": {
+    "id": "6880664a-7c74-4591-959e-66733807bed7",
+    "name": "康生同志、谢富治付总理、杨成武代总长和北京航空学院《红旗》战斗队代表谈话纪要",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "康生 谢富治 杨成武",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/6880664a-7c74-4591-959e-66733807bed7/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/6880664a-7c74-4591-959e-66733807bed7/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "康生同志、谢富治付总理、杨成武代总长和北京航空学院《红旗》战斗队代表谈话纪要",
+        "authors": [
+          "康生",
+          "谢富治",
+          "杨成武"
+        ],
+        "page_start": 1,
+        "page_end": 2,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 10,
+            "day": 6
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "6880664a-7c74-4591-959e-66733807bed7",
+  "resource_type": "book",
+  "version": 2
+}
