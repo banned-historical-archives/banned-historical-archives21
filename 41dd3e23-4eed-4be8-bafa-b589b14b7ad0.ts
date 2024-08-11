e45c18addabf19887942225f754d3fed50c1b9c2
Your Name <you@example.com>
2024-08-11 20:21:47 +0800

    1

---
+++ 41dd3e23-4eed-4be8-bafa-b589b14b7ad0.ts
@@ -0,0 +1,47 @@
+export default {
+  "entity": {
+    "id": "41dd3e23-4eed-4be8-bafa-b589b14b7ad0",
+    "name": "在社会主义的中国怀疑一切决不是无产阶级的战斗口号",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "鲁学迅",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/41dd3e23-4eed-4be8-bafa-b589b14b7ad0/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/41dd3e23-4eed-4be8-bafa-b589b14b7ad0/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "在社会主义的中国怀疑一切决不是无产阶级的战斗口号",
+        "authors": [
+          "鲁学迅"
+        ],
+        "page_start": 1,
+        "page_end": 2,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 9,
+            "day": 25
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "41dd3e23-4eed-4be8-bafa-b589b14b7ad0",
+  "resource_type": "book",
+  "version": 2
+}
