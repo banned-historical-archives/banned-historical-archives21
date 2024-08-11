c11d5e5ec54a1cb7c9c748108223ea658273c9e3
Your Name <you@example.com>
2024-08-11 10:50:32 +0800

    1

---
+++ b310162f-3f09-4798-b6f5-94cfbf9a0269.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "b310162f-3f09-4798-b6f5-94cfbf9a0269",
+    "name": "陈毅副总理的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "陈毅",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/b310162f-3f09-4798-b6f5-94cfbf9a0269/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "陈毅副总理的讲话",
+        "authors": [
+          "陈毅"
+        ],
+        "page_start": 1,
+        "page_end": 1,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": []
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "b310162f-3f09-4798-b6f5-94cfbf9a0269",
+  "resource_type": "book",
+  "version": 2
+}
