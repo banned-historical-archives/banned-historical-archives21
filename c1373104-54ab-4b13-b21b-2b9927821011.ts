acd63e4f3e2f180758e757f71ea50864327fa9db
Your Name <you@example.com>
2024-08-11 10:57:32 +0800

    1

---
+++ c1373104-54ab-4b13-b21b-2b9927821011.ts
@@ -0,0 +1,53 @@
+export default {
+  "entity": {
+    "id": "c1373104-54ab-4b13-b21b-2b9927821011",
+    "name": "访问“红旗”杂志编辑部纪要",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "北京政法学院红卫兵伏虎战斗队 北京师范大学图书馆革命文献资料组 北京师范大学红色勤务站",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/c1373104-54ab-4b13-b21b-2b9927821011/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/c1373104-54ab-4b13-b21b-2b9927821011/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/c1373104-54ab-4b13-b21b-2b9927821011/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/c1373104-54ab-4b13-b21b-2b9927821011/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/c1373104-54ab-4b13-b21b-2b9927821011/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/c1373104-54ab-4b13-b21b-2b9927821011/6.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "访问“红旗”杂志编辑部纪要",
+        "authors": [
+          "北京政法学院红卫兵伏虎战斗队",
+          "北京师范大学图书馆革命文献资料组",
+          "北京师范大学红色勤务站"
+        ],
+        "page_start": 1,
+        "page_end": 6,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 10,
+            "day": 22
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "c1373104-54ab-4b13-b21b-2b9927821011",
+  "resource_type": "book",
+  "version": 2
+}
