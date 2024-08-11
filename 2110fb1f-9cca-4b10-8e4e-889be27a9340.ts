a87e4ba838412ac861be25a7d7174dc4df6d6ef9
Your Name <you@example.com>
2024-08-11 19:30:38 +0800

    1

---
+++ 2110fb1f-9cca-4b10-8e4e-889be27a9340.ts
@@ -0,0 +1,48 @@
+export default {
+  "entity": {
+    "id": "2110fb1f-9cca-4b10-8e4e-889be27a9340",
+    "name": "吴德、关锋同志九月廿四日凌晨在地质学院《东方红》公社揭发批判鄒家龙大会上的重要讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "吴德 关锋",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/2110fb1f-9cca-4b10-8e4e-889be27a9340/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/2110fb1f-9cca-4b10-8e4e-889be27a9340/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "吴德、关锋同志九月廿四日凌晨在地质学院《东方红》公社揭发批判鄒家龙大会上的重要讲话",
+        "authors": [
+          "吴德",
+          "关锋"
+        ],
+        "page_start": 1,
+        "page_end": 2,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 9,
+            "day": 24
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "2110fb1f-9cca-4b10-8e4e-889be27a9340",
+  "resource_type": "book",
+  "version": 2
+}
