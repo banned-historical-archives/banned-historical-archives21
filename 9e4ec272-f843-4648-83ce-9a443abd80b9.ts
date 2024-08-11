c957ee86c14ec6e768394134d67ce6ba3f52b2a8
Your Name <you@example.com>
2024-08-12 06:12:37 +0800

    1

---
+++ 9e4ec272-f843-4648-83ce-9a443abd80b9.ts
@@ -0,0 +1,18 @@
+export default {
+  "entity": {
+    "id": "9e4ec272-f843-4648-83ce-9a443abd80b9",
+    "name": "1973年12月讲话参考材料",
+    "internal": false,
+    "type": "db",
+    "official": false,
+    "author": "李德生 陈锡联 杨得志",
+    "files": []
+  },
+  "parser_option": {
+    "articles": []
+  },
+  "parser_id": "result-json-v2",
+  "path": "9e4ec272-f843-4648-83ce-9a443abd80b9.json",
+  "resource_type": "book",
+  "version": 2
+}
