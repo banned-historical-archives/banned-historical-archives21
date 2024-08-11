36edcd7b299fac160aebac2751f26a4438dc07d2
Your Name <you@example.com>
2024-08-11 12:29:04 +0800

    1

---
+++ 3931ccf1-f175-4f4d-bc82-4f8894d0613b.ts
@@ -0,0 +1,48 @@
+export default {
+  "entity": {
+    "id": "3931ccf1-f175-4f4d-bc82-4f8894d0613b",
+    "name": "在八届十一中全会上林彪同志的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "林彪",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/3931ccf1-f175-4f4d-bc82-4f8894d0613b/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/3931ccf1-f175-4f4d-bc82-4f8894d0613b/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/3931ccf1-f175-4f4d-bc82-4f8894d0613b/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "在八届十一中全会上林彪同志的讲话",
+        "authors": [
+          "林彪"
+        ],
+        "page_start": 1,
+        "page_end": 3,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 11,
+            "day": 20
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "3931ccf1-f175-4f4d-bc82-4f8894d0613b",
+  "resource_type": "book",
+  "version": 2
+}
