b8ec0c23aa241b7cab0e1d8d79410797b9ca706e
Your Name <you@example.com>
2024-08-11 12:23:48 +0800

    1

---
+++ 82cabf7d-b2da-4763-8bb0-363d26e4ca1d.ts
@@ -0,0 +1,38 @@
+export default {
+  "entity": {
+    "id": "82cabf7d-b2da-4763-8bb0-363d26e4ca1d",
+    "name": "中央军委指示",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/82cabf7d-b2da-4763-8bb0-363d26e4ca1d/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "中央军委指示",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": []
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "82cabf7d-b2da-4763-8bb0-363d26e4ca1d",
+  "resource_type": "book",
+  "version": 2
+}
