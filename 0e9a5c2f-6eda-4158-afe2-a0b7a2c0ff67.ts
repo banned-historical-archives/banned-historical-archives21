c3527809ed12345f1c2be39eca96c394269181d4
Your Name <you@example.com>
2024-08-11 11:01:43 +0800

    1

---
+++ 0e9a5c2f-6eda-4158-afe2-a0b7a2c0ff67.ts
@@ -0,0 +1,48 @@
+export default {
+  "entity": {
+    "id": "0e9a5c2f-6eda-4158-afe2-a0b7a2c0ff67",
+    "name": "康生同志七月二十八日讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "康生",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/0e9a5c2f-6eda-4158-afe2-a0b7a2c0ff67/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/0e9a5c2f-6eda-4158-afe2-a0b7a2c0ff67/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/0e9a5c2f-6eda-4158-afe2-a0b7a2c0ff67/3.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "康生同志七月二十八日讲话",
+        "authors": [
+          "康生"
+        ],
+        "page_start": 1,
+        "page_end": 3,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 7,
+            "day": 28
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "0e9a5c2f-6eda-4158-afe2-a0b7a2c0ff67",
+  "resource_type": "book",
+  "version": 2
+}
