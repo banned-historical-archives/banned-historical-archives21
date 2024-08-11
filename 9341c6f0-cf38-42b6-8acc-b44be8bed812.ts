644715c59d8b9af54dcce62f9d66732507301d28
Your Name <you@example.com>
2024-08-11 20:14:21 +0800

    1

---
+++ 9341c6f0-cf38-42b6-8acc-b44be8bed812.ts
@@ -0,0 +1,46 @@
+export default {
+  "entity": {
+    "id": "9341c6f0-cf38-42b6-8acc-b44be8bed812",
+    "name": "在接见外地来京革命师生大会上周恩来同志的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "周恩来",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/9341c6f0-cf38-42b6-8acc-b44be8bed812/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "在接见外地来京革命师生大会上周恩来同志的讲话",
+        "authors": [
+          "周恩来"
+        ],
+        "page_start": 1,
+        "page_end": 1,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 8,
+            "day": 31
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "9341c6f0-cf38-42b6-8acc-b44be8bed812",
+  "resource_type": "book",
+  "version": 2
+}
