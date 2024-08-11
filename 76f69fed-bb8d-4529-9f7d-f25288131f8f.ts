87c40b9880a1c7c9814f087a94b3cc691469f84e
Your Name <you@example.com>
2024-08-11 19:36:26 +0800

    1

---
+++ 76f69fed-bb8d-4529-9f7d-f25288131f8f.ts
@@ -0,0 +1,38 @@
+export default {
+  "entity": {
+    "id": "76f69fed-bb8d-4529-9f7d-f25288131f8f",
+    "name": "中央文化革命小组成员",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/76f69fed-bb8d-4529-9f7d-f25288131f8f/1.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "中央文化革命小组成员",
+        "authors": [],
+        "page_start": 1,
+        "page_end": 1,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": []
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "76f69fed-bb8d-4529-9f7d-f25288131f8f",
+  "resource_type": "book",
+  "version": 2
+}
