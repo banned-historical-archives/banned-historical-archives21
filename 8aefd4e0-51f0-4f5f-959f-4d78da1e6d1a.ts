2c28ef102d8e44e693bd7b9a55c9be51e6d85e0f
Your Name <you@example.com>
2024-08-11 11:55:22 +0800

    1

---
+++ 8aefd4e0-51f0-4f5f-959f-4d78da1e6d1a.ts
@@ -0,0 +1,45 @@
+export default {
+  "entity": {
+    "id": "8aefd4e0-51f0-4f5f-959f-4d78da1e6d1a",
+    "name": "揭穿资本家代理人刘少奇的反动面目——刘少奇在东亚毛纺厂罪行的调查",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "新北大公社物理系战斗团第四野战军、政法大队联合调查组",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8aefd4e0-51f0-4f5f-959f-4d78da1e6d1a/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8aefd4e0-51f0-4f5f-959f-4d78da1e6d1a/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8aefd4e0-51f0-4f5f-959f-4d78da1e6d1a/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8aefd4e0-51f0-4f5f-959f-4d78da1e6d1a/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8aefd4e0-51f0-4f5f-959f-4d78da1e6d1a/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8aefd4e0-51f0-4f5f-959f-4d78da1e6d1a/6.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "揭穿资本家代理人刘少奇的反动面目——刘少奇在东亚毛纺厂罪行的调查",
+        "authors": [
+          "新北大公社物理系战斗团第四野战军、政法大队联合调查组"
+        ],
+        "page_start": 1,
+        "page_end": 6,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": []
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "8aefd4e0-51f0-4f5f-959f-4d78da1e6d1a",
+  "resource_type": "book",
+  "version": 2
+}
