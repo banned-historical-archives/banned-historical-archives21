84e4042f6a10a1c37df46a2fc0d192a0c1084685
Your Name <you@example.com>
2024-08-11 20:11:07 +0800

    1

---
+++ 375ddbec-9bac-445e-82f8-39b22b7ea999.ts
@@ -0,0 +1,47 @@
+export default {
+  "entity": {
+    "id": "375ddbec-9bac-445e-82f8-39b22b7ea999",
+    "name": "在接见外地来京革命师生大会上林彪同志的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "林彪",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/375ddbec-9bac-445e-82f8-39b22b7ea999/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/375ddbec-9bac-445e-82f8-39b22b7ea999/2.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "在接见外地来京革命师生大会上林彪同志的讲话",
+        "authors": [
+          "林彪"
+        ],
+        "page_start": 1,
+        "page_end": 2,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": [
+          {
+            "year": 1966,
+            "month": 8,
+            "day": 31
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "375ddbec-9bac-445e-82f8-39b22b7ea999",
+  "resource_type": "book",
+  "version": 2
+}
