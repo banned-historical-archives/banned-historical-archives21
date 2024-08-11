7d213754d972f3d8f4ae170530d4ddeb6b3ec09b
Your Name <you@example.com>
2024-08-11 12:35:09 +0800

    1

---
+++ 0859a0b0-aaa5-4e50-a7af-fb5e6c39e3ae.ts
@@ -0,0 +1,48 @@
+export default {
+  "entity": {
+    "id": "0859a0b0-aaa5-4e50-a7af-fb5e6c39e3ae",
+    "name": "陶铸同志的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "陶铸",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/0859a0b0-aaa5-4e50-a7af-fb5e6c39e3ae/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/0859a0b0-aaa5-4e50-a7af-fb5e6c39e3ae/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/0859a0b0-aaa5-4e50-a7af-fb5e6c39e3ae/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/0859a0b0-aaa5-4e50-a7af-fb5e6c39e3ae/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/0859a0b0-aaa5-4e50-a7af-fb5e6c39e3ae/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/0859a0b0-aaa5-4e50-a7af-fb5e6c39e3ae/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/0859a0b0-aaa5-4e50-a7af-fb5e6c39e3ae/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/0859a0b0-aaa5-4e50-a7af-fb5e6c39e3ae/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/0859a0b0-aaa5-4e50-a7af-fb5e6c39e3ae/9.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "陶铸同志的讲话",
+        "authors": [
+          "陶铸"
+        ],
+        "page_start": 1,
+        "page_end": 9,
+        "ocr": {
+          "use_onnx": true,
+          "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+          "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+        },
+        "dates": []
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "0859a0b0-aaa5-4e50-a7af-fb5e6c39e3ae",
+  "resource_type": "book",
+  "version": 2
+}
