61a8a17de064601113a88086e90a812c99f821a3
Your Name <you@example.com>
2024-10-03 01:57:07 +0800

    1

---
+++ 8e8ca834-e81e-42b1-8b91-274964ea5be6.ts
@@ -0,0 +1,70 @@
+export default {
+  "entity": {
+    "id": "8e8ca834-e81e-42b1-8b91-274964ea5be6",
+    "name": "“四人帮”及其党羽柳肖李乔破坏科学技术工作的部分反动言论",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "中国科学院运动办公室",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/12.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/13.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/14.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/15.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/16.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/17.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/18.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/19.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/20.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/21.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/22.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/23.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/24.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/25.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/26.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/27.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/28.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/29.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives21/main/8e8ca834-e81e-42b1-8b91-274964ea5be6/30.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "“四人帮”及其党羽柳肖李乔破坏科学技术工作的部分反动言论",
+        "authors": [
+          "中国科学院运动办公室"
+        ],
+        "page_start": 1,
+        "page_end": 30,
+        "dates": [
+          {
+            "year": 1977,
+            "month": 11
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "8e8ca834-e81e-42b1-8b91-274964ea5be6",
+  "resource_type": "book",
+  "version": 2
+}
